2b6969afbdb39d29ba1c6d5befc128c646f27a54
arisen37 <arisen2356@gmail.com>
2025-08-21 17:48:29 +0000

    logout added

--- auth.js
+++ auth.js
@@ -3,20 +3,16 @@ require('dotenv').config();
 const JWT_SECRET = process.env.JWT_SECRET;
 
 function authenticateToken(req, res, next) {
-    
-    const token = req.cookies.token;
+    const token = req.headers.token;
 
     if (!token) {
         return res.status(401).json({ message: "Access denied. No token provided." });
     }
 
     try {
-        
         const decodedUserInfo = jwt.verify(token, JWT_SECRET);
-        
-        req.user = decodedUserInfo; 
-        
-        next(); 
+        req.user = decodedUserInfo;
+        next();
     } catch (error) {
         return res.status(403).json({ message: "Invalid or expired token." });
     }
